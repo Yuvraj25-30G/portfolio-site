f209c09323e361fb102d8dfe67468bbf301a13e0
yuvraj <yuvrajsaxena2015@gmail.com>
2023-03-11 21:13:24 +0530

    update procfilr

--- resume_demo/resume_demo/settings.py
+++ resume_demo/resume_demo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3qydz53ik2l_qvdwo16%uywl0fn#g$dahd0235p7!m*l%ke8cg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
